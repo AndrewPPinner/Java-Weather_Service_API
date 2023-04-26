c2344c6c7305400cb6078e82330b7dd8c47839ca
AndrewPPinner <andppinner@gmail.com>
2023-04-25 20:48:08 -0400

    Testing new CD

--- src/main/java/JavaRestAPI/restAPI/WeatherController.java
+++ src/main/java/JavaRestAPI/restAPI/WeatherController.java
@@ -36,7 +36,7 @@ public class WeatherController {
             low = today.select("[style='display:inline']").get(3).text();
             icon = today.select(".wob_tci").attr("src");
             condition =  today.select(".wob_tci").attr("alt");
-            System.out.print("Testing");
+            System.out.print("Testing The following update");
         } catch (IOException e) {
             e.printStackTrace();
         }
